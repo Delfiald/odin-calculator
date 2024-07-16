880d94b94b520bacabb12e4e1f8dbac762ed0baa
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2024-07-16 15:21:18 +0700

    Initial Design
    make initial Design
    for now only have 4 operators
    with 10 numbers 0-9
    and a point (.) and equals
    
    thinking about some features:
    -history

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const result = document.querySelector('.calc-result');
+
+result.addEventListener('click', (e) => {
+  const target = e.target;
+
+  switch(target.id){
+    case 'history-btn':
+      result.classList.toggle('history');
+      break;
+    default:
+      console.log("nothing");
+      break;
+  }
+})
